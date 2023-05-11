098aca511bf416ed1b45075b954fb17d8d6d6aed
mindy schwabenbauer <mindy.schwabenbauer@gmail.com>
2023-05-11 14:38:37 -0700

    passport implementation: updated user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
+
+// const userSchema = new mongoose.Schema({
+//     email: { type: String, required: true, unique: true },
+//     password: { type: String, required: true, minlength: 8 },
+// });
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 8 },
 });
+
+// add passport plug in for user schema (adds support for username and password)
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 
 module.exports = mongoose.model('User', userSchema);
